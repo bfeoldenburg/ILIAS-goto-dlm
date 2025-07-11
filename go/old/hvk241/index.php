1194508ff4d157b2fe61bd5dbeabb7ebd0397274
Manfred Kellermann <m.kellermann@bfe.de>
2025-07-11 11:01:37 +0200

    folder in history 2

--- go/old/hvk241/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Weiterleitungsziel inkl. 301 Status
-        header("Location: https://goto.bfe-elearning.de/goto.php?target=svy_2786&client_id=c-il-goto&v=pa_obj_sv", true, 301);
-	exit;
-?>
